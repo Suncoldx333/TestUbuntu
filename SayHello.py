e77e27ccf0de0e0e28103178c0e94ecf7014e604
WangZhaoyun <wangzhaoyunx@163.com>
2024-07-30 14:49:44 +0800

    add discord

--- SayHello.py
+++ SayHello.py
@@ -1,6 +1,16 @@
 import datetime
+import discord
+
 
 def showTime():
     time = datetime.datetime.now()
     formatted_now = time.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_now
+
+
+def showVersion():
+    version = discord.__version__
+    #print(version)
+    return version
+
+#showVersion()
